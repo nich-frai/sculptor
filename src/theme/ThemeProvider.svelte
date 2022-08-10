36812331e8beac6060620362dfbab94eedda87f1
Nicholas Frai <107412954+nich-frai@users.noreply.github.com>
2022-08-10 19:35:20 -0300

    theme provider tweak

--- src/theme/ThemeProvider.svelte
+++ src/theme/ThemeProvider.svelte
@@ -41,7 +41,7 @@
   :global(:where(html)) {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
       Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-    font-size: 13px;
+    font-size: 11px;
     accent-color: var(--color-primary);
     color: var(--text-color-on-surface);
   }
