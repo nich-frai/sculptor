1986ece152c8dde6e4e22c798580b43769601124
Nicholas Frai <107412954+nich-frai@users.noreply.github.com>
2022-08-16 00:53:47 -0300

    predictable pagetitle size

--- src/components/PageTitle.svelte
+++ src/components/PageTitle.svelte
@@ -1,3 +1,4 @@
+
 <h1>
   <div class="title-decoration" />
   {#if $$slots.icon}
@@ -19,11 +20,11 @@
 <style>
   h1 {
 		position: relative;
-    margin: 1rem 0;
+    margin: 0.5rem 0;
 		display: flex	;
     padding: 0;
-		column-gap: 0.75rem;
-		font-size: var(--page-title-size, var(--text-size-5, 2rem));
+		column-gap: 1rem;
+		font-size: calc( var(--page-title-size, var(--text-size-5, 2rem)) *0.85);
 		line-height: var(--page-title-size, 1.6rem);
 		align-items: center;
 		vertical-align: middle;
@@ -33,9 +34,9 @@
 	}
 	.title-decoration {
 		position: relative;
-		height: calc(var(--page-title-size, 1.6rem) * 1.6) ;
-		width: 4pt;
-		flex-basis: 4pt;
+		height: calc(var(--page-title-size, 1.8rem)) ;
+		width: var(--page-title-decoration-width, 4pt);
+		flex-basis: var(--page-title-decoration-width, 4pt);
 		border-radius: var(--radius-1);
 		background-color: var(--color-primary);
 	}
